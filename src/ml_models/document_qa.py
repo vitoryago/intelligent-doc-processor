ebccbcb65fc278f6dd99e682d48d58f6ff856afe
Vitor Yago <vitor.yagocp@gmail.com>
2025-03-10 19:54:00 -0300

    document qa

---
+++ src/ml_models/document_qa.py
@@ -0,0 +1,7 @@
+"""
+document_qa.py
+
+This module adds document question-answering capabilities using LangChain.
+It creates a system that can answer specific questions about documents
+by understanding their content and retrieving relevant information.
+"""
